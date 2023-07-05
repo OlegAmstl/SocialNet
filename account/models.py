614fd2816816020bb292fc02a653b027d5fec2b0
Oleg <amstl@mail.ru>
2023-07-05 17:49:27 +0700

    Модель Contact для подписки на пользователей.

--- account/models.py
+++ account/models.py
@@ -17,3 +17,33 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'Профиль {self.user.username}'
+
+
+class Contact(models.Model):
+    '''
+    Промежуточная модель для форимирования связей между пользователями.
+    '''
+
+    user_from = models.ForeignKey(User,
+                                  on_delete=models.CASCADE,
+                                  related_name='rel_from_set')
+    user_to = models.ForeignKey(User,
+                                on_delete=models.CASCADE,
+                                related_name='rel_to_set')
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'{self.user_from} подписан на {self.user_to}'
+
+
+User.add_to_class('following',
+                  models.ManyToManyField('self',
+                                         through=Contact,
+                                         related_name='followers',
+                                         symmetrical=False))
