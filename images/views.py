c4edbc2feeb34fcefb82dc123a21c8d9532a9bc7
Oleg <amstl@mail.ru>
2023-07-16 16:55:12 +0700

    С помощью Redis можно посмотреть рейтинг самых просматриваемых изображений.

--- images/views.py
+++ images/views.py
@@ -8,6 +8,14 @@ from django.views.decorators.http import require_POST
 from .forms import ImageCreateForm
 from .models import Image
 from actions.utils import create_action
+import redis
+from django.conf import settings
+
+r = redis.Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB
+)
 
 
 @login_required
@@ -37,8 +45,11 @@ def image_detail(request, id, slug):
     Представлнение детальной информации о изображении.
     '''
     image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr(f'image:{image.id}:views')
+    r.zincrby('image_ranking', 1, image.id)
     return render(request, 'images/image/detail.html',
-                  {'section': 'images', 'image': image})
+                  {'section': 'images', 'image': image,
+                   'total_views': total_views})
 
 
 @login_required
@@ -85,3 +96,21 @@ def image_list(request):
                       {'section': 'images', 'images': images})
     return render(request, 'images/image/list.html',
                   {'section': 'images', 'images': images})
+
+
+@login_required
+def image_ranking(request):
+    '''
+    Представление наиболее просматреваемого изображения.
+    '''
+    # Получаем словарь рейтинга изображений.
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # Получаем наиболее просматриваемые изображения.
+    most_viewed = list(Image.objects.filter(
+        id__in=image_ranking_ids
+    ))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request, 'images/image/ranking.html',
+                  {'section': 'images',
+                   'most_viewed': most_viewed})
